a05ba0edb05840066155f08cacebaf0803b40185
skyfall947 <atencio947@gmail.com>
2021-02-12 12:43:58 -0400

    8.  Char sum problem solved

---
+++ 1. Basics of Input And Output/8. Char Sum/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int weight = 0;
+	char S[100];
+	
+	scanf("%s", S);
+	for (int i = 0; i < strlen(S); ++i)
+	{
+		//printf("%i\n", S[i]-96); // print weights
+		weight += S[i]-96;
+	}
+	printf("%i\n", weight);
+	return 0;
+}
